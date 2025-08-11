958399f9c7061497cef4bc658a717ed1d0468c45
keigokudo <keigokudo55@googlemail.com>
2025-08-11 20:17:38 +0100

    minimize the css load per a page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    cssChunking: true, // load only the CSS that is needed for the current page
+  },
 };
 
 export default nextConfig;
